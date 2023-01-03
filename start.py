65b2af4c0319c7038a30ca7f79ec2989b87e3157
Roman Itskov <75908271+RomiillST@users.noreply.github.com>
2023-01-03 14:21:13 +0200

    Update start.py

--- start.py
+++ start.py
@@ -7,5 +7,5 @@ dis = upd.dispatcher
 dis.add_handler(CommandHandler(command='start', callback=start))
 dis.add_handler(CommandHandler(command='ultaaa', callback=imba))
 dis.add_handler(MessageHandler(filters=Filters.text, callback=text_answer))
-upd.start_polling(drop_pending_updates=True)
+upd.start_polling(сlean=True)
 upd.idle()
